5565e54ac1cbd1930cdd18e913a478178d6df039
Gabriel de Oliveira <gabriel-ao@hotmail.com>
2019-12-04 12:52:10 -0300

    Primeira conexão LojaVirtual

---
+++ LojaVirtualV2/LojaVirtualV2/Controllers/ProdutoController.cs
@@ -0,0 +1,27 @@
+﻿using LojaVirtualV2.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LojaVirtualV2.Controllers
+{
+    public class ProdutoController : Controller
+    {
+        public ActionResult Visualizar()
+        {
+            Produto produto = GetProduto();
+
+            return View(produto);
+           // return new ContentResult() { Content = "<h1>Produto -> Visualizar</h1>", ContentType = "text/html" };
+        }
+
+        private Produto GetProduto()
+        {
+            return new Produto()
+            {
+                Id = 1,
+                Nome = "Xbox one X",
+                Descricao = "Jogue em 4K",
+                Valor = 2000.00M
+            };
+        }
+    }
+}
